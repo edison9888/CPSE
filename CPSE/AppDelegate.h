49ea4f0a9d79b7a97ea319cf38a3b16c1590e1ca
meekr <perry.lei@gmail.com>
2013-07-08 14:04:55 +0800

    Initial Commit

---
+++ CPSE/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CPSE
+//
+//  Created by Lei Perry on 7/8/13.
+//  Copyright (c) 2013 BitRice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
