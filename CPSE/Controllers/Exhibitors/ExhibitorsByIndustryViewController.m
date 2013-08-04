1136c73ba6f3c0111bc244b1010ab4ed3097d26e
meekr <perry.lei@gmail.com>
2013-08-04 19:41:40 +0800

    search by industry api

--- CPSE/Controllers/Exhibitors/ExhibitorsByIndustryViewController.m
+++ CPSE/Controllers/Exhibitors/ExhibitorsByIndustryViewController.m
@@ -8,6 +8,7 @@
 
 #import "ExhibitorsByIndustryViewController.h"
 #import "UIColor+BR.h"
+#import "ExhibitorListViewController.h"
 
 @interface ExhibitorsByIndustryViewController ()
 {
@@ -37,7 +38,6 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
     [self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
 }
 
@@ -66,6 +66,11 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *dict = _data[indexPath.row];
+    NSString *query = [NSString stringWithFormat:@"search&type=class&q=%@", dict[@"id"]];
+    ExhibitorListViewController *vc = [[ExhibitorListViewController alloc] initWithAction:query];
+    vc.title = dict[@"name"];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 @end
