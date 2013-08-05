afc676fb481d4eabcf50377a544dfed0e127380e
jimji2008 <jimji2008@Jis-MacBook-Air.local>
2013-08-05 19:28:24 +1000

    fix comments layout error during loading period

---
+++ CPSE/Controllers/News/CommentEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommentEditorViewController.h
+//  CPSE
+//
+//  Created by Ji Jim on 8/5/13.
+//  Copyright (c) 2013 BitRice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommentEditorViewController : UIViewController
+
+@end
