256e4719c11beb90f6d1e3aff19e1f46f6808820
meekr <perry.lei@gmail.com>
2013-08-08 14:21:32 +0800

    introduction

--- CPSE/Controllers/Intro/ExhibitsScopeViewController.m
+++ CPSE/Controllers/Intro/ExhibitsScopeViewController.m
@@ -14,5 +14,18 @@
 
 @implementation ExhibitsScopeViewController
 
+- (void)loadView {
+    [super loadView];
+
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    scrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:scrollView];
+    
+    UIImage *image = [UIImage imageNamed:@"exhibitors-areas-range"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    [scrollView addSubview:imageView];
+    scrollView.contentSize = image.size;
+}
 
 @end
