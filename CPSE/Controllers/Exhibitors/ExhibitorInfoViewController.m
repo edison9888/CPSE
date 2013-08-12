baae45c0f08b0c9b0a5deb8953d10b8750e1a6ea
meekr <perry.lei@gmail.com>
2013-08-12 17:03:09 +0800

    display exhibitor logo

--- CPSE/Controllers/Exhibitors/ExhibitorInfoViewController.m
+++ CPSE/Controllers/Exhibitors/ExhibitorInfoViewController.m
@@ -8,6 +8,7 @@
 
 #import "ExhibitorInfoViewController.h"
 #import "UIColor+BR.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface ExhibitorInfoViewController ()
 {
@@ -17,6 +18,9 @@
     UIScrollView *_scrollView;
     UILabel *_infoLabel;
     UILabel *_descLabel;
+
+    UIImageView *_logoView;
+    UIActivityIndicatorView *_activityIndicator;
 }
 @end
 
@@ -35,6 +39,14 @@
     _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
     _scrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.view addSubview:_scrollView];
+    
+    _logoView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 20, 300, 80)];
+    _logoView.contentMode = UIViewContentModeScaleAspectFit;
+    [_scrollView addSubview:_logoView];
+    
+    _activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    _activityIndicator.frame = _logoView.frame;
+    [_scrollView addSubview:_activityIndicator];
 
     _infoLabel = [[UILabel alloc] init];
     _infoLabel.backgroundColor = [UIColor clearColor];
@@ -54,6 +66,7 @@
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
+    [_activityIndicator startAnimating];
     [AFClient getPath:[NSString stringWithFormat:@"api.php?action=ccinfo&id=%d", _id]
            parameters:nil
               success:^(AFHTTPRequestOperation *operation, id JSON) {
@@ -67,7 +80,26 @@
 
 - (void)populateInterface {
     CGRect rect = _scrollView.bounds;
-    CGFloat topOffset = 20.f;
+    
+    // set logo
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_data[@"logo"]]];
+    [req addValue:@"image/*" forHTTPHeaderField:@"Accept"];
+    __block UIImageView *imageView = _logoView;
+    __block UIActivityIndicatorView *indicator = _activityIndicator;
+    [_logoView setImageWithURLRequest:req
+                      placeholderImage:nil
+                               success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image){
+                                   imageView.image = image;
+                                   [indicator stopAnimating];
+                                   [indicator removeFromSuperview];
+                               }
+                               failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error){
+                                   DLog(@"error: %@", [error description]);
+                                   [indicator stopAnimating];
+                                   [indicator removeFromSuperview];
+                               }];
+    
+    CGFloat topOffset = 110.f;
     
     NSMutableString *info = [NSMutableString string];
     [info appendFormat:@"企业名称：%@\n", _data[@"name"]];
